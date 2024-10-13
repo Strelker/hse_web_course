7875a6d84002702190ce62bf723c538d812b30da
Ilya Afonichev <afonichev.ila@yandex.ru>
2024-10-14 00:02:05 +0300

    1 task

---
+++ sandbox/src/task1-even-calc/index.js
@@ -0,0 +1,9 @@
+export const solutionFn = (numbers) => {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 === 0) {
+            sum += numbers[i];
+        }
+    }
+    return sum;
+};
